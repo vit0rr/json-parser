675fe6aa6dafbab551ede1a4286190ab053dfb0c
TK <leandrotk100@gmail.com>
2023-09-02 14:42:18 -0300

    Add more tests

--- src/__tests__/lexer.test.ts
+++ src/__tests__/lexer.test.ts
@@ -2,7 +2,9 @@ import { lexer } from '../lexer.js';
 
 describe('lexer.ts', () => {
     it('returns a valid token', () => {
-        const tokens = lexer('{"name":"Vitor","age":18}');
+        const tokens = lexer(
+            '{"name":"Vitor","age":18,"isHuman":true,"isRobot":false}'
+        );
         expect(tokens).toEqual([
             { type: 'LEFT_BRACE', value: undefined },
             { type: 'STRING', value: 'name' },
@@ -12,6 +14,14 @@ describe('lexer.ts', () => {
             { type: 'STRING', value: 'age' },
             { type: 'COLON', value: undefined },
             { type: 'NUMBER', value: '18' },
+            { type: 'COMMA', value: undefined },
+            { type: 'STRING', value: 'isHuman' },
+            { type: 'COLON', value: undefined },
+            { type: 'TRUE', value: undefined },
+            { type: 'COMMA', value: undefined },
+            { type: 'STRING', value: 'isRobot' },
+            { type: 'COLON', value: undefined },
+            { type: 'FALSE', value: undefined },
             { type: 'RIGHT_BRACE', value: undefined },
         ]);
     });
