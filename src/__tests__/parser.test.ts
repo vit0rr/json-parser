a49082517e7d08e9c4d0657c55e730d519eda519
TK <leandrotk100@gmail.com>
2023-09-02 15:39:23 -0300

    Improve parser test

--- src/__tests__/parser.test.ts
+++ src/__tests__/parser.test.ts
@@ -3,8 +3,12 @@ import { parser } from '../parser.js';
 
 describe('parser.ts', () => {
     it('returns a valid parsed json', () => {
-        const tokens = lexer('{"name":"Vitor","age":18}');
+        const tokens = lexer(
+            '{"name":"Vitor","age":18,"isHuman":true,"isRobot":false}'
+        );
+
         const json = parser(tokens);
+
         expect(json).toEqual({
             type: 'Program',
             body: [
@@ -31,6 +35,28 @@ describe('parser.ts', () => {
                             value: {
                                 type: 'NumberLiteral',
                                 value: '18',
+                            },
+                        },
+                        {
+                            type: 'Property',
+                            key: {
+                                type: 'STRING',
+                                value: 'isHuman',
+                            },
+                            value: {
+                                type: 'BooleanLiteral',
+                                value: true,
+                            },
+                        },
+                        {
+                            type: 'Property',
+                            key: {
+                                type: 'STRING',
+                                value: 'isRobot',
+                            },
+                            value: {
+                                type: 'BooleanLiteral',
+                                value: false,
                             },
                         },
                     ],
